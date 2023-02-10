41937b730901e44a70d58a1273201c34b9b83358
Valerie Tokareva <tokareva@sports.ru>
2023-02-10 13:52:10 +0400

    added TODO about more comprehensive checks

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 
 // TODO: fix all warnings and error from ts and Vetur
+// TODO Check on a real phones and in browsers under Windows
 import './assets/main.css';
 
 const app = createApp(App);
